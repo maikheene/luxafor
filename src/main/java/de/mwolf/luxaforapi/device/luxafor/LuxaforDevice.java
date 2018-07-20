6a8dbd121e6bed1e697434c6ea3555301fe892d7
Maik Wolf <maik.wolf@posteo.de>
2018-07-20 22:02:23 +0200

    Create LuxaforDevice with the Luxafor specification
    Also I added methods send simple commands to the luxafor:
    * setColor - Set the color
    * fadeColor - Fades to the color with the given speed

---
+++ src/main/java/de/mwolf/luxaforapi/device/luxafor/LuxaforDevice.java
@@ -0,0 +1,51 @@
+package de.mwolf.luxaforapi.device.luxafor;
+
+import de.mwolf.luxaforapi.color.Color;
+import de.mwolf.luxaforapi.command.ColorCommand;
+import de.mwolf.luxaforapi.command.FadeCommand;
+import de.mwolf.luxaforapi.device.Device;
+
+import javax.usb.UsbDevice;
+
+public class LuxaforDevice extends Device {
+
+    public static final int LUXAFOR_USB_INTERFACE = 0;
+    public static final int LUXAFOR_USB_ENDPOINT = 1;
+
+    public LuxaforDevice(UsbDevice usbDevice) {
+        super(usbDevice);
+    }
+
+    public void setColor(final Led led, final Color color) {
+        sendCommand(new ColorCommand(led, color));
+    }
+
+    public void fadeColor(final Led led, final Color color, final Integer speed) {
+        sendCommand(new FadeCommand(led, color, speed));
+    }
+
+    @Override
+    public byte getUsbInterfaceNumber() {
+        return LUXAFOR_USB_INTERFACE;
+    }
+
+    @Override
+    public byte getUsbEndpointNumber() {
+        return LUXAFOR_USB_ENDPOINT;
+    }
+
+    @Override
+    public boolean detachDeviceDriver() {
+        return true;
+    }
+
+    @Override
+    public short getVendor() {
+        return (short) 0x04d8;
+    }
+
+    @Override
+    public short getProduct() {
+        return (short) 0xf372;
+    }
+}
