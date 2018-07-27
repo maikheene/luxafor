44ec9ac69ab9e6ef4a7b44c99fa066bb0204de25
Maik Wolf <maik.wolf@posteo.de>
2018-07-27 16:58:04 +0200

    Create new method to get RGB from hex Code
    Also I added new Color

--- src/main/java/de/mwolf/luxaforapi/color/Colors.java
+++ src/main/java/de/mwolf/luxaforapi/color/Colors.java
@@ -2,8 +2,27 @@ package de.mwolf.luxaforapi.color;
 
 public class Colors {
 
-    public static Color RED = new Color((byte) 255,  (byte) 0, (byte) 0);
-    public static Color GREEN = new Color((byte) 0,  (byte) 255, (byte) 0);
-    public static Color BLUE = new Color((byte) 0,  (byte) 0, (byte)255);
+    public static Color RED = hex2Rgb("#FF0000");
+    public static Color GREEN = hex2Rgb("#00FF00");
+    public static Color BLUE = hex2Rgb("#0000FF");
+    public static Color YELLOW = hex2Rgb("#FFFF00");
+    public static Color CYAN = hex2Rgb("#00FFFF");
+    public static Color MAGENTA = hex2Rgb("#FF00FF");
+
+
+
+    /**
+     *
+     *
+     * @param colorStr e.g. "#FFFFFF"
+     * @return
+     */
+    public static Color hex2Rgb(String colorStr) {
+        return new Color(
+                Integer.valueOf( colorStr.substring( 1, 3 ), 16 ).byteValue(),
+                Integer.valueOf( colorStr.substring( 3, 5 ), 16 ).byteValue(),
+                Integer.valueOf( colorStr.substring( 5, 7 ), 16 ).byteValue()
+        );
+    }
 
 }
